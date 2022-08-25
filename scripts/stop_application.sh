65770f036c6f441bbbd9bc395eb9d09ddf0911f2
Aashima-sharma <57859863+Aashima-sharma@users.noreply.github.com>
2022-08-25 18:59:19 +0530

    Update stop_application.sh

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ps -ef | grep tomcat10.0.8 | grep -v grep
+# ps -ef | grep tomcat10.0.8 | grep -v grep
 [ $?  -eq "0" ] && echo "process is running" && service tomcat stop|| echo "process is not running"
